ae4120feb3f099c58faa019e248a0b5cc52f6d4e
mcmicko <mcmzrc@gmail.com>
2021-11-05 16:48:39 +0100

    Implement Card & Navbar Comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,33 @@
+import { useState } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">hello world</div>;
-}
+import Card from "./components/card/Card";
+import Navbar from "./components/navbar/Navbar";
+
+const App = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState("");
+
+  console.log(user);
+  return (
+    <div className="container">
+      {user ? (
+        <>
+          <Navbar />
+          <Card />
+        </>
+      ) : (
+        <div className="login">
+          <input
+            type="text"
+            placeholder="username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <button onClick={(e) => setUser(username)}>login</button>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
